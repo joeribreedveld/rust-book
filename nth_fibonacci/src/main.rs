b053e32eaabf4f4e84a692aaf55b089e5004773f
joeribreedveld <joeri.breedveld123@gmail.com>
2025-01-04 09:55:59 +0100

    non recursive fibonacci met range

--- nth_fibonacci/src/main.rs
+++ nth_fibonacci/src/main.rs
@@ -8,17 +8,45 @@ fn main() {
     println!("Number: {result}");
 }
 
+// fn fibonacci_recursive(n: i32) -> u64 {
+//     if n < 0 {
+//         panic!("Negative number!");
+//     }
+
+//     match n {
+//         0 => 0,
+//         1 | 2 => 1,
+//         3 => 2,
+//         _ => fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2),
+//     }
+// }
+
 fn fibonacci(n: i32) -> u64 {
     if n < 0 {
         panic!("Negative number!");
+    } else if n == 0 {
+        panic!("Zero is not possible!");
+    } else if n == 1 {
+        return 1;
     }
 
-    match n {
-        0 => 0,
-        1 | 2 => 1,
-        3 => 2,
-        _ => fibonacci(n - 1) + fibonacci(n - 2),
+    // Totaal
+    let mut current = 0;
+
+    // Parent
+    let mut grandparent = 0;
+    let mut parent = 1;
+
+    // Begin bij 1 want eerste iteratie staat al klaar in variable initialisatie
+    for _i in 1..n {
+        // Tel de parent en grandparent bij elkaar op
+        current = grandparent + parent;
+        grandparent = parent;
+        parent = current;
     }
+
+    // Return current van vorige niet huidige
+    current
 }
 
 fn get_num() -> i32 {
