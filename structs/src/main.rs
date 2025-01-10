f1226e9a37272be247da3d0b911e4103d27f6929
joeribreedveld <joeri.breedveld123@gmail.com>
2025-01-10 21:21:33 +0100

    progress update

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -5,6 +5,9 @@ struct User {
     sign_in_count: u64,
 }
 
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
 fn build_user(email: String, username: String) -> User {
     User {
         active: true,
@@ -20,5 +23,11 @@ fn main() {
 
     let user1 = build_user(example_email.to_string(), example_username);
 
-    println!("{:?}", user1.username);
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 }
