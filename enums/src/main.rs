75343f7db0b78b17d6af07085f6999d6ab13d535
joeribreedveld <joeri.breedveld123@gmail.com>
2025-01-12 18:53:00 +0100

    start on enums work

---
+++ enums/src/main.rs
@@ -0,0 +1,14 @@
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+fn main() {
+    let home = IpAddr::V4(127, 0, 0, 1);
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    let some_number = Some(5);
+    let some_char = Some('e');
+
+    let absent_number: Option<i32> = None;
+}
