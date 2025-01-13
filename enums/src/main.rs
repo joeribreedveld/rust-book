74922e5b2e335dea56c02d7664bc582fc137a367
joeribreedveld <joeri.breedveld123@gmail.com>
2025-01-13 22:11:40 +0100

    update finish c6

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,14 +1,21 @@
-enum IpAddr {
-    V4(u8, u8, u8, u8),
-    V6(String),
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
 }
 
 fn main() {
-    let home = IpAddr::V4(127, 0, 0, 1);
-    let loopback = IpAddr::V6(String::from("::1"));
+    let coin = Coin::Penny;
 
-    let some_number = Some(5);
-    let some_char = Some('e');
-
-    let absent_number: Option<i32> = None;
+    value_in_cents(coin);
 }
